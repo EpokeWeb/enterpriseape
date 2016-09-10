3987c4e3566d30d8f807f1f4122fa9f224756689
Francis Campbell <franciscampbel@hotmail.com>
2016-09-10 19:48:00 -0400

    search button

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Enterpriseape
   class Application < Rails::Application
+  	config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
