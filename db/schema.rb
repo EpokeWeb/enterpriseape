72ede3802095644373b5e6383356f94f04ccdb18
Francis Campbell <franciscampbel@hotmail.com>
2016-09-11 16:40:26 -0400

    fin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911141724) do
+ActiveRecord::Schema.define(version: 20160911201622) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160911141724) do
     t.integer  "invoice_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal  "price"
     t.index ["invoice_id"], name: "index_purchases_on_invoice_id"
   end
 
